fbd0b1391a42f69d8dfae83a24591c3a959f5abc
niyujoshi11 <125252213+niyujoshi11@users.noreply.github.com>
2023-09-29 18:09:50 +0530

    Add files via upload

---
+++ d3_task.py
@@ -0,0 +1,9 @@
+a = (input("enter your name: "))
+b = int(input("enter your age: "))
+c = 18 - (b)
+
+if (b >= 18):
+    print("you are elligable for driving licence")
+else:
+    print("you are elligable then",c,"years")
+    
